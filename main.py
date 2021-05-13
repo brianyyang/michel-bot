8967248a5f451527ed2e57998684c18529d776dc
brianyyang <b.y.yang212@gmail.com>
2021-05-13 19:44:50 -0400

    added jocelyn reactions

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ import os
 # initialize client
 client = discord.Client()
 
+#################### MAIN CALLBACKS ###################
+
 # callback for when bot is online
 @client.event
 async def on_ready():
@@ -25,6 +27,25 @@ async def on_message(message):
 
     if message.content.startswith('!hello'):
         await message.channel.send('Hello!')
+
+    await scary_jocey_react(message)
+
+##################### HELPER FUNCTIONS ##################
+
+# determines if i should react to jocelyn's message with the scaryjocey emote
+async def scary_jocey_react(message):
+    source = message.author
+    if f'{source.name}#{source.discriminator}' == 'jocelyn#5644' or 'jocelyn' in message.content.lower():
+        scary_jocey_emote = await get_emoji('scaryjocey', message.guild)
+        await message.add_reaction(scary_jocey_emote)
+
+# finds a custom emoji on the server and returns it
+async def get_emoji(emoji_name, guild):
+     emoji_list = guild.emojis
+     for emoji in emoji_list:
+         if emoji.name == emoji_name:
+             return emoji
+
 
 # runs the bot with its login token
 client.run(os.environ['TOKEN'])
