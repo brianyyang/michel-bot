a7acb3ac0b57ba72ab01eeeedaac4ccd71bbc1d6
brianyyang <b.y.yang212@gmail.com>
2021-05-13 16:48:13 -0400

    commented out local .env file load of variables

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 ''' a lot of these comments are gonna be redundant but it will help readability tbh '''
 
 
-# load environment variables into os
-load_dotenv()
+# load environment variables into os if ran locally
+# load_dotenv()
 
 # initialize client
 client = discord.Client()
